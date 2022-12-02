21135c1c3f530b6531d66c6ae4029e74e3a776a2
Matias Juarez <flipeador@outlook.com>
2022-12-01 23:20:46 -0300

    commit

---
+++ README.md
@@ -0,0 +1,209 @@
+# Node.js PromiseEx
+
+Provides extended promise functionality and utilities.
+
+## Installation
+
+```
+npm install https://github.com/flipeador/node.js-promise-ex
+```
+
+## Examples
+
+<details>
+<summary><h5>Promise object with extended functionality</h5></summary>
+
+`PromiseEx` allows to set a **timeout** and add **events** for when the promise resolves, rejects or expires.
+
+```js
+const { PromiseEx } = require('@flipeador/node.js-promise-ex');
+
+function onEvent(value)
+{
+    switch (this.state)
+    {
+        case 'fulfilled':
+            console.log(`The promise has been resolved with:`, value);
+            break;
+        case 'rejected':
+            console.log(`The promise has been rejected with:`, value.message);
+            break;
+        default: // timeout
+            // By default, when a promise expires, it is rejected with PromiseTimeout error.
+            // This behavior can be changed by calling this#resolve or this#reject before returning.
+            //this.resolve('I don\'t want an error!');
+            console.log(`The promise has expired after ${value} ms`);
+            break;
+    }
+}
+
+(async () => {
+    const promise = new PromiseEx((resolve, reject) => {
+        // never explicitly resolves nor rejects
+    }, { timeout: 1000, onEvent });
+
+    try {
+        await promise; // throws after 1000 ms
+    } catch (error) {
+        console.log(error); // PromiseTimeout [Error]
+    }
+
+    console.log('Promise state:', promise.state); // 'rejected'
+})();
+```
+
+```
+The promise has expired after 1000 ms
+The promise has been rejected with: Promise timed out after 1000 ms
+PromiseTimeout [Error]: Promise timed out after 1000 ms
+    ...
+Promise state: rejected
+```
+
+</details>
+
+<details>
+<summary><h5>Omit callback function and set arguments</h5></summary>
+
+When `PromiseEx` is instantiated, properties `resolve` and `reject` are set. This allows the callback function to be omitted.
+
+In addition, a list of arguments can be specified in the options.
+
+```js
+const { PromiseEx } = require('@flipeador/node.js-promise-ex');
+
+(async () => {
+    const promise = new PromiseEx({
+        onResolve: function(value, hello, world) {
+            this.result = `${hello} ${world}${value}`; // (1)
+        },
+        args: ['Hello', 'World']
+    });
+    promise.resolve('!'); // (2)
+    console.log('Resolve:', await promise); // (2)
+    console.log('Result:', promise.result); // (1)
+})();
+```
+
+```js
+Resolve: !
+Result: Hello World!
+```
+
+</details>
+
+<details>
+<summary><h5>Subtle difference between Promise and PromiseEx</h5></summary>
+
+`PromiseEx` supports async callback functions, this means that any unhandled error is catched and rejected instead of triggering an [unhandled promise rejection](https://nodejs.org/api/process.html#event-unhandledrejection).
+
+```js
+const process = require('node:process');
+const { PromiseEx } = require('@flipeador/node.js-promise-ex');
+
+/**
+ * Emitted whenever a Promise is rejected and no error handler is attached to the promise.
+ */
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled promise rejection:', promise);
+});
+
+(async () => {
+    try {
+        await new PromiseEx(async () => {
+            throw new Error('My Error!'); // --> (1)
+        });
+    } catch (error) { // <-- (1)
+        console.log('Rejected:', error);
+    }
+
+    console.log('-'.repeat(50));
+
+    try {
+        await new Promise(async () => {
+            throw new Error('My Error!'); // unhandled promise rejection
+        });
+    } catch (error) {
+        console.log('Rejected:', error);
+    }
+})();
+```
+
+```
+Rejected: Error: My Error!
+    ...
+--------------------------------------------------
+Unhandled promise rejection: Promise {
+  <rejected> Error: My Error!
+      ...
+}
+```
+
+</details>
+
+<details>
+<summary><h5>Control the concurrency of an async function</h5></summary>
+
+```js
+const { PromiseEx, PromiseSync } = require('@flipeador/node.js-promise-ex');
+
+const psync = new PromiseSync();
+
+async function asyncFn(index)
+{
+    console.log(await new Promise(async (resolve) => {
+        await PromiseEx.sleep(index & 1 ? 100 : 50);
+        resolve(`asyncFn: Index #${index}`)
+    }));
+}
+
+async function asyncSyncFn(index)
+{
+    console.log(await psync.run(async (resolve) => {
+        await PromiseEx.sleep(index & 1 ? 100 : 50);
+        resolve(`asyncSyncFn: Index #${index}`)
+    }));
+}
+
+(async () => {
+    for (let i = 1; i <= 5; ++i)
+        asyncFn(i);
+
+    await PromiseEx.sleep(1000); console.log('-'.repeat(50));
+
+    for (let i = 1; i <= 5; ++i)
+        asyncSyncFn(i);
+
+    await PromiseEx.sleep(1000); console.log('-'.repeat(50));
+
+    const asyncSyncFn2 = PromiseSync.wrap(asyncFn);
+    for (let i = 1; i <= 5; ++i)
+        asyncSyncFn2(`${i} (PromiseSync.wrap)`);
+})();
+```
+
+```
+asyncFn: Index #2
+asyncFn: Index #4
+asyncFn: Index #1
+asyncFn: Index #3
+asyncFn: Index #5
+--------------------------------------------------
+asyncSyncFn: Index #1
+asyncSyncFn: Index #2
+asyncSyncFn: Index #3
+asyncSyncFn: Index #4
+asyncSyncFn: Index #5
+--------------------------------------------------
+asyncFn: Index #1 (PromiseSync.wrap)
+asyncFn: Index #2 (PromiseSync.wrap)
+asyncFn: Index #3 (PromiseSync.wrap)
+asyncFn: Index #4 (PromiseSync.wrap)
+asyncFn: Index #5 (PromiseSync.wrap)
+```
+
+</details>
+
+## License
+
+This project is licensed under the **GNU General Public License v3.0**. See the [license file](LICENSE) for details.
---
+++ index.js
@@ -0,0 +1,193 @@
+﻿'use strict';
+
+const { setTimeout } = require('node:timers');
+
+function call(fn)
+{
+    try { fn(); }
+    catch (error) { console.error(error); }
+}
+
+class PromiseError extends Error { }
+
+class PromiseTimeout extends PromiseError
+{
+    constructor(timeout)
+    {
+        super(`Promise timed out after ${timeout} ms`);
+    }
+}
+
+/**
+ * Promise object with extended functionality.
+ * The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+ */
+class PromiseEx extends Promise
+{
+    /** @type {'pending'|'fulfilled'|'rejected'} */
+    state = 'pending';
+
+    /**
+     * Create a PromiseEx object.
+     * @param {Function} callback `Function(resolveFn, rejectFn)`.
+     * @param {Object} options Options.
+     * @param {Number} options.timeout Rejects with {@link PromiseTimeout} once the timeout expires.
+     * @param {Function} options.onResolve Function called when the promise is fulfilled.
+     * @param {Function} options.onReject Function called when the promise is rejected.
+     * @param {Function} options.onTimeout Function called when the promise has expired.
+     * @param {Function} options.onEvent Function called when the promise resolves, rejects or expires.
+     * @param {Array} options.args List of arguments that receive the events.
+     */
+    constructor(callback, options)
+    {
+        if (typeof(callback) !== 'function' && options === undefined)
+            options = callback, callback = undefined;
+
+        const obj = {};
+        super((resolve, reject) => Object.assign(obj, {resolve, reject}));
+
+        if (options?.timeout)
+            this._timer = setTimeout(() => {
+                this.#emit('onTimeout', options.timeout, options);
+                this.reject(new PromiseTimeout(options.timeout));
+            }, options.timeout);
+
+        this.resolve = (value) => {
+            if (this.state === 'pending')
+            {
+                obj.resolve(value);
+                this.#settled('fulfilled', 'onResolve', value, options);
+            }
+        };
+
+        this.reject = (error) => {
+            if (this.state === 'pending')
+            {
+                obj.reject(error);
+                this.#settled('rejected', 'onReject', error, options);
+            }
+        };
+
+        if (callback !== undefined)
+        {
+            (async () => {
+                try {
+                    await callback.call(this, this.resolve, this.reject);
+                } catch (error) {
+                    this.reject(error);
+                }
+            })();
+        }
+    }
+
+    static resolve(x, options)
+    {
+        if (typeof(x?.then) !== 'function')
+            return super.resolve(x);
+        return new this(x.then.bind(x), options);
+    }
+
+    static sleep = require('node:util').promisify(setTimeout);
+
+    #emit(event, value, options)
+    {
+        call(() => options?.[event]?.call(this, value, ...options?.args??[]));
+        call(() => options?.onEvent?.call(this, value, ...options?.args??[]));
+    }
+
+    #settled(state, event, value, options)
+    {
+        this.state = state;
+
+        this.#emit(event, value, options);
+
+        this.resolve = () => undefined;
+        this.reject = () => undefined;
+
+        clearTimeout(this._timer);
+        delete this._timer;
+    }
+}
+
+/**
+ * Control the concurrency of an async function.
+ */
+class PromiseSync
+{
+    promise = Promise.resolve()
+
+    /**
+     * Execute a function and block until it returns, resolves or rejects.
+     * The `callback` is executed after the previous callback has returned.
+     * @param {Function} callback `Function(resolve,reject)` to execute.
+     * @param {Boolean} resolveOnReturn Whether to resolve when `callback` returns.
+     * @return Returns the resolved value or the result of `callback`.
+     */
+    run(callback, resolveOnReturn)
+    {
+        return this.#execute(callback, resolveOnReturn);
+    }
+
+    /**
+     * Try to execute a function and block until it returns, resolves or rejects.
+     * The `callback` is executed after the previous callback has returned.
+     * @param {Function} callback `Function(resolve,reject)` to execute.
+     * @param {Boolean} resolveOnReturn Whether to resolve when `callback` returns.
+     * @return Returns the resolved value or the result of `callback`.
+     * @remarks Any errors are ignored and set as resolved.
+     */
+    try(callback, resolveOnReturn)
+    {
+        return this.#execute(callback, resolveOnReturn, true);
+    }
+
+    #execute(callback, resolveOnReturn, resolveOnError)
+    {
+        if (typeof(callback) !== 'function')
+            throw PromiseError(`Invalid callback function: ${callback}`);
+        return new Promise((resolve, reject) => {
+            const reject2 = resolveOnError ? resolve : reject;
+            this.promise = this.promise.then(() => {
+                return new Promise(async (_resolve) => {
+                    try {
+                        const retval = await callback(
+                            (value) => _resolve(resolve(value)),
+                            (value) => _resolve(reject(value))
+                        );
+                        if (resolveOnReturn) _resolve(resolve(retval));
+                    } catch (error) {
+                        _resolve(reject2(error))
+                    }
+                });
+            });
+        });
+    }
+
+    /**
+     * Wrap an async function to prevent multiple simultaneous invocations.
+     * @param {Function} fn The async function to wrap.
+     * @param {Function?} callback Function to be called before blocking.
+     * @param {Function?} check Function to check the previous result and determine whether to continue executing `fn`.
+     * @return {Function} Returns an async function.
+     */
+    static wrap(fn, callback, check)
+    {
+        let promise = Promise.resolve();
+        let cache = { result: undefined };
+        return async (...args) => {
+            if (callback) await callback(...args);
+            return promise = promise.then(async () => {
+                if (cache && (!check || await check(cache.result)))
+                    return cache.result = await fn(...args);
+                else cache = undefined;
+            });
+        };
+    }
+}
+
+module.exports = {
+    PromiseError,
+    PromiseTimeout,
+    PromiseEx,
+    PromiseSync
+};
